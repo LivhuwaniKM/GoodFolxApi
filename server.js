f962d657ba590e99fb5f0d13dd496e1503977f95
LivhuwaniKM <lkmasindi1208@gmail.com>
2025-06-06 15:42:00 +0200

    server conf setting

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB error:', err));
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => console.log('MongoDB connected'))
+//   .catch(err => console.log('MongoDB error:', err));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log('MongoDB error:', err));
 
 // Schema
 const ContactSchema = new mongoose.Schema({
